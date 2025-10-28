0c39decce391fcf239422a93004ebb441f2854da
nabilnajwan <132110723+nabilnajwan@users.noreply.github.com>
2025-10-28 14:33:25 +0800

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,21 @@
 import 'package:flutter/material.dart';
 import 'package:petfoodplanner/petfoodplannerscreen.dart';
 
+//main() function to run app
 void main() {
   runApp(const MainApp());
 }
-
+//provide base design
 class MainApp extends StatelessWidget {
   const MainApp({super.key});
 
+//splash screen
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(home: SplashScreen());
   }
 }
-
+//state change
 class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
 
@@ -26,7 +28,9 @@ class _SplashScreenState extends State<SplashScreen> {
   @override
   void initState() {
     super.initState();
+    //wait 2 second before navigate to the next screen
     Future.delayed(const Duration(seconds: 2), () {
+      //to go to my main screen
       Navigator.pushReplacement(
         context,
         MaterialPageRoute(builder: (context) => const Petfoodplannerscreen()),
@@ -38,10 +42,13 @@ class _SplashScreenState extends State<SplashScreen> {
     return Scaffold(
       body: Center(
         child: Column(
+          //center the position
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
+            //put title text
             Text(
               "PET FOOD PLANNER",
+              //title text decoration
               style: TextStyle(
                 fontSize: 24,
                 fontWeight: FontWeight.bold,
@@ -49,6 +56,7 @@ class _SplashScreenState extends State<SplashScreen> {
               ),
             ),
             SizedBox(height: 10),
+            //loading animation
             CircularProgressIndicator(),
             SizedBox(height: 20),
           ],
@@ -57,3 +65,4 @@ class _SplashScreenState extends State<SplashScreen> {
     );
   }
 }
+
